42e55e369ea5b76b262a9a7b72592d0a2181caa7
yash001dev <y.sanghavi001@gmail.com>
2021-10-04 09:08:05 +0530

    simple authentication addedd

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem "sqlite3", "~> 1.4"
 gem "puma", "~> 5.0"
 gem "rack-cors"
 gem "devise"
+gem "simple_token_authentication"
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 # gem 'jbuilder', '~> 2.7'
 # Use Redis adapter to run Action Cable in production
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_04_014440) do
+ActiveRecord::Schema.define(version: 2021_10_04_032525) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "first_name"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2021_10_04_014440) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "authentication_token", limit: 30
+    t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
