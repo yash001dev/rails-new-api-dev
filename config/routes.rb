ada19128a48da342d6435520f73c9ed89afaf4e6
yash001dev <y.sanghavi001@gmail.com>
2021-10-04 07:23:39 +0530

    Basic devise Setup completed

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  namespace :v1 do
+    resources :contacts
+    resources :sessions, only: [:create, :destroy]
+  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
